765daddfd1204c9402ab7e25d8711a9c99dd5dab
Cloud <roney.mendes@live.com>
2023-05-07 13:18:16 -0300

    Inicio da criação do contexto de AUTH

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
+# MiniJ&T
+*Este e um projeto de estudos.
 
 
 
+<img src="./public/logo512.png" alt="Logo do Meu Projeto" width="200" />
 
-
-
-# Bibliotecas
+## Bibliotecas
 - React router DOM
 - Firebase
 - Styled-component
 - React icon
 - Lottie web
 - json-loader
+
+
+
+## Ambiente de Produção
+- https://app.netlify.com/
+
+## Notas
+- O arquivo .eslintrc e para que os warning do codigos sejam ignorados.
+
+### Estudos
+- 07/05 Contexto de autenticação
--- src/services/firebase/configs.js
+++ src/services/firebase/configs.js
@@ -3,12 +3,24 @@ import { getFirestore } from 'firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtalNWTdAIWdGV_2p5t2zz6g__z2GcSqc",
-  authDomain: "new-mini-blog.firebaseapp.com",
-  projectId: "new-mini-blog",
-  storageBucket: "new-mini-blog.appspot.com",
-  messagingSenderId: "238472190628",
-  appId: "1:238472190628:web:6744b2530ee8b51c8f6431"
+  // apiKey: "AIzaSyDtalNWTdAIWdGV_2p5t2zz6g__z2GcSqc",
+  // authDomain: "new-mini-blog.firebaseapp.com",
+  // projectId: "new-mini-blog",
+  // storageBucket: "new-mini-blog.appspot.com",
+  // messagingSenderId: "238472190628",
+  // appId: "1:238472190628:web:6744b2530ee8b51c8f6431"
+
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_API_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_API_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_API_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_API_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_APP_ID
+
+  
+
+
+
 };;
 
 // Initialize Firebase
