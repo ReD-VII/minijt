d019a69425f7fc537670ad05f1d0cfbc880b4870
Cloud <roney.mendes@live.com>
2023-05-14 15:55:46 -0300

    Up Data

--- src/pages/CreatePost/index.jsx
+++ src/pages/CreatePost/index.jsx
@@ -1,36 +1,87 @@
 import React, { useState } from 'react'
 
 
+import { useNavigate } from 'react-router-dom'
+
+import { useAuthValue } from '../../context/AuthContext'
+import { CreateFormStyleConteiner } from './styles'
+import { FormConteiner } from '../../styles/Theme/forms'
+
+
 
 
 const CreatePost = () => {
-    const [config, setConfig] = useState({
-        readonly: false, // torna o editor somente leitura
-        toolbar: true, // exibe a barra de ferramentas
-        enableDragAndDropFileToEditor: true, // permite arrastar e soltar arquivos no editor
-        spellcheck: true, // habilita verificação ortográfica
-        showCharsCounter: true, // exibe o contador de caracteres
-        askBeforePasteHTML: false, // desabilita o aviso de segurança ao colar conteúdo HTML
-        buttons: ['bold', 'italic', 'underline', 'strikethrough', 'image', 'link'] // exibe somente os botões selecionados
-    })
 
+  const [title, setTitle] = useState('')
+  const [image, setImage] = useState('')
+  const [body, setBody] = useState('')
+  const [tags, setTags] = useState([])
+  const [ rage, setRage ] = useState(10)
+  const [formError, setFormError] = useState('')
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+  }
 
-    }
+  return (
+    <>
+      <h1 style={{
+        textAlign: 'center',
+        marginTop: '10px',
+      }}>Criar post</h1>
+      <FormConteiner>
+        <div className='form_conteiner'>
+          <form >
+            <div className='FormGroup'>
+              <label>
+                <span>Título</span>
+                <input type='text' name='title' required onChange={(e) => setTitle(e.target.value)} value={title} />
+              </label>
+            </div>
+            <div className='FormGroup'>
+              <label>
+                <span>URL da imagem</span>
+                <input type='text' name='title' required onChange={(e) => setImage(e.target.value)} value={image} />
+              </label>
+            </div>
+            <div className='FormGroup'>
+              <label>
+                <span>Rage: {rage}</span>
+                {/* <span className='rage'></span> */}
+                <input type='range' name='title' required onChange={(e) => setRage(e.target.value)} value={rage} />
+              </label>
+            </div>
+            <div className='FormGroup'>
+              <label>
+                <span>Conteúdo</span>
+                <textarea className="meu-textarea" name='body' required onChange={(e) => setBody(e.target.value)} value={body} />
+              </label>
+            </div>
+            <div className='FormGroup'>
+              <label>
+                <span>Tags</span>
+                <input type='text' name='Tags' required onChange={(e) => setTags(e.target.value)} value={tags} />
+              </label>
+            </div>
+            
 
-    return (
-        <div>
-            <JoditEditor
-                value={conteudo}
-                config={config}
-                tabIndex={1}
-                onBlur={handleChange}
-            />
+            {/* {!loading && <input className='btn_forms' type="submit" value='Enviar' />}
+            {loading && <input className='btn_forms' type="submit" value='aguarde...' disabled />} */}
+            {/* <input className='btn_forms' type="submit" value='Postar...' /> */}
+
+            <div className='btn_conteiner'>
+
+              <button className='btn_forms' name='' type='submit'>Postar</button>
+            </div>
+          </form>
+          {/* {error &&
+          <p className='erro_msg'>{error}</p>
+        } */}
         </div>
-    )
+      </FormConteiner>
+    </>
+  )
 }
 
 export default CreatePost
